cba35ae6a7d7e4e6ca8a570b226aef6f026d36de
BinEP <bradystoffel@gmail.com>
2015-02-13 14:57:20 -0600

    Removed unused Class and changed Directions

--- src/gameActions/Directions.java
+++ src/gameActions/Directions.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 
 public abstract class Directions extends Control {
 
-	public enum Direction {
-		
-		up, down, left, right;
-	}
+	
 	
 	public Directions() {
 		
@@ -24,46 +21,6 @@ public abstract class Directions extends Control {
 	public abstract void leftReleased();
 	public abstract void rightReleased();
 	
-	public ArrayList<Direction> nextDirection = new ArrayList<Direction>();
-	
-	public void addDirection(Direction d) {
-
-		nextDirection.add(d);
-
-		// lastDirection = d;
-
-	}
-
-	
-	public void executeDirection() {
-
-		
-		Direction d = nextDirection.get(0);
-		nextDirection.remove(0);
-
-		switch (d) {
-
-		case up:
-
-			up();
-
-			break;
-		case down:
-			down();
-
-			break;
-		case left:
-
-			left();
-
-			break;
-		case right:
-
-			right();
-
-		}
-
-	}
 	
 	
 }
