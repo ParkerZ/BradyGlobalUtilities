2fe318548c92dc3d26af2cf9779964eabefbed8c
BinEP <bradystoffel@gmail.com>
2015-02-22 00:09:32 -0600

    Utility class updated with added folder param

--- src/utilityClasses/Panel.java
+++ src/utilityClasses/Panel.java
@@ -1,6 +1,6 @@
 package utilityClasses;
 import java.awt.Color;
-import java.awt.Graphics2D;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -62,7 +62,7 @@ public class Panel extends JPanel implements ActionListener, KeyListener {
 
 	}
 
-	public void paintComponent(Graphics2D g) {
+	public void paintComponent(Graphics g) {
 
 		super.paintComponent(g);
 		g.setColor(Color.WHITE);
